24b166198010fe5c268a4a3b9e6f122dfcde1f9d
Sayre Blades <sayre.blades@gmail.com>
2018-04-29 13:14:40 -0400

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+install_requires = [
+    'click',
+    'cloudpickle',
+    'boto3',
+    'docker',
+    ]
 
 setup(name='sprite2',
       packages=['sprite2'],
+      install_requires=install_requires,
       tests_require=[
         'pytest',
       ],
