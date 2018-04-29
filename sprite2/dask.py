b47b901061271a8bccf84b430a4177c301cf289e
Sayre Blades <sayre.blades@gmail.com>
2018-04-28 22:57:16 -0400

    fix all the problems

--- sprite2/dask.py
+++ sprite2/dask.py
@@ -1,8 +1,10 @@
 from functools import partial
 import logging
-import concurrent
+import concurrent.futures
+import sys
 
 from dask.local import get_async
+import boto3
 
 import sprite2
 
@@ -10,55 +12,62 @@ import sprite2
 logger = logging.getLogger(__name__)
 
 
-def apply_sync_lambda(
-        func,
-        args=(),
-        kwds={},
-        callback=None,
+def apply_sync_lambda_factory(
         invoker=sprite2.aws.remote,
-        lambda_name='sprite',
         ):
-    key, task_info, dumps, loads, get_id, pack_exception = args
-    func_wrapper = sprite2.aws.remote_invoke(
-        func=func,
-        lambda_name=lambda_name,
-        invoker=invoker,
-        request_id=key)
-    res = func_wrapper(*args, **kwds)
-    if callback is not None:
-        callback(res)
+    def inner(
+            func,
+            args=(),
+            kwds={},
+            callback=None,
+            ):
+        key, _, _, _, _, _ = args
+        logger.debug(f"executing task: {key}")
+        func_wrapper = sprite2.aws.remote_invoke(
+            func=func,
+            invoker=invoker,
+            request_id=key)
+        try:
+            res = func_wrapper(*args, **kwds)
+        except BaseException as e:
+            result = e, sys.exc_info()[2]
+            failed = True
+            res = key, result, failed
+        if callback is not None:
+            callback(res)
+            logger.debug(f'callback with result {str(res)[:100]}')
+    return inner
 
 
-def apply_async_lambda(
-        func,
-        args=(),
-        kwds={},
-        callback=None,
+def apply_async_lambda_factory(
         executor=None,
         invoker=sprite2.aws.remote,
-        lambda_name='sprite',
         ):
-    key, task_info, dumps, loads, get_id, pack_exception = args
-    task = lambda: apply_sync_lambda(func, args, kwds, callback, invoker, lambda_name)  # noqa
-    executor.submit(task)
+    def inner(
+            func,
+            args=(),
+            kwds={},
+            callback=None,
+            ):
+        key, _, _, _, _, _ = args
+        task = lambda: apply_sync_lambda_factory(invoker)(func, args, kwds, callback)  # noqa
+        executor.submit(task)
+    return inner
 
 
 def get(dsk, keys, **kwargs):
     num_workers = kwargs.pop('num_workers', 1000)
     lambda_name = kwargs.pop('lambda_name', 'sprite')
     invoker = kwargs.pop('invoker', sprite2.aws.remote)
+    invoker = partial(invoker, lambda_name=lambda_name)
     if num_workers and num_workers > 1:
         executor = concurrent.futures.ThreadPoolExecutor(num_workers)
-        apply = partial(
-            apply_async_lambda,
+        apply = apply_async_lambda_factory(
             executor=executor,
             invoker=invoker,
-            lambda_name=lambda_name,
             )
     else:
-        apply = partial(
-            apply_sync_lambda,
+        apply = apply_sync_lambda_factory(
             invoker=invoker,
-            lambda_name=lambda_name,
             )
     return get_async(apply, num_workers, dsk, keys, **kwargs)
